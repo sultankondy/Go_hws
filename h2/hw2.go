41caaabca185c37834b4ca07a8da34d5bda71ebc
sltnkondy <sltnkondy@gmail.com>
2022-02-26 08:19:48 +0600

    prepare file

---
+++ h2/hw2.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	world := struct {
+		English string
+		Spanish string
+		French  string
+	}{
+		"world",
+		"mundo",
+		"monde",
+	}
+	fmt.Printf("Hello, %s/%s/%s!", world.English, world.Spanish, world.French)
+}
+
+// Price is the cost of something in US cents.
+type Price int64
+
+// String is the string representation of a Price
+// These should be represented in US Dollars
+// Example: 2595 cents => $25.95
+func (p Price) String() string {
+	return ""
+}
+
+// Prices is a map from an item to its price.
+var Prices = map[string]Price{
+	"eggs":          219,
+	"bread":         199,
+	"milk":          295,
+	"peanut butter": 445,
+	"chocolate":     150,
+}
+
+// RegisterItem adds the new item in the prices map.
+// If the item is already in the prices map, a warning should be displayed to the user,
+// but the value should be overwritten.
+// Bonus (1pt) - Use the "log" package to print the error to the user
+func RegisterItem(prices map[string]Price, item string, price Price) {
+	// TODO
+}
+
+// Cart is a struct representing a shopping cart of items.
+type Cart struct {
+	Items      []string
+	TotalPrice Price
+}
+
+// hasMilk returns whether the shopping cart has "milk".
+func (c *Cart) hasMilk() bool {
+	// TODO
+	return false
+}
+
+// HasItem returns whether the shopping cart has the provided item name.
+func (c *Cart) HasItem(item string) bool {
+	// TODO
+	return false
+}
+
+// AddItem adds the provided item to the cart and update the cart balance.
+// If item is not found in the prices map, then do not add it and print an error.
+// Bonus (1pt) - Use the "log" package to print the error to the user
+func (c *Cart) AddItem(item string) {
+	// TODO
+}
+
+// Checkout displays the final cart balance and clears the cart completely.
+func (c *Cart) Checkout() {
+	// TODO
+}
